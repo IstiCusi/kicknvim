120b2294cc9a3a8bbf1a10095da8fd03acef88ac
Stephan Strauss <StephanStrauss@gmx.ch>
2025-05-29 15:10:28 +0200

    Name changes to standardize kicknvim

---
+++ plugin/kicknvim.lua
@@ -0,0 +1,14 @@
+local kickass = require("kicknvim")
+
+vim.api.nvim_create_user_command("KickAssemble", kicknvim.assemble_current_file, {})
+vim.api.nvim_create_user_command("KickRun", kicknvim.run_prg, {})
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "kickass",
+  callback = function(args)
+    local keys = kickass.get_config().keys
+    vim.keymap.set("n", keys.assemble, kicknvim.assemble_current_file, { buffer = args.buf, desc = "Assemble with KickAss" })
+    vim.keymap.set("n", keys.run, kicknvim.run_prg, { buffer = args.buf, desc = "Run PRG with VICE" })
+  end,
+})
