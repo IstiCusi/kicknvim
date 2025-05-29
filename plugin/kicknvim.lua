0b4a59c2eea103af4ffda30b109e7843da3b542a
Stephan Strauss <StephanStrauss@gmx.ch>
2025-05-29 15:25:57 +0200

    Naming bug fix

--- plugin/kicknvim.lua
+++ plugin/kicknvim.lua
@@ -1,4 +1,4 @@
-local kickass = require("kicknvim")
+local kicknvim = require("kicknvim")
 
 vim.api.nvim_create_user_command("KickAssemble", kicknvim.assemble_current_file, {})
 vim.api.nvim_create_user_command("KickRun", kicknvim.run_prg, {})
@@ -7,7 +7,7 @@ vim.api.nvim_create_user_command("KickRun", kicknvim.run_prg, {})
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "kickass",
   callback = function(args)
-    local keys = kickass.get_config().keys
+    local keys = kicknvim.get_config().keys
     vim.keymap.set("n", keys.assemble, kicknvim.assemble_current_file, { buffer = args.buf, desc = "Assemble with KickAss" })
     vim.keymap.set("n", keys.run, kicknvim.run_prg, { buffer = args.buf, desc = "Run PRG with VICE" })
   end,
